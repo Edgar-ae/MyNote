f269917dedb831948649c9fd64c7cd42539e2dae
Edgar-ae <mantoplonia@gmail.com>
2020-06-30 11:54:48 -0500

    primer comit

---
+++ script.js
@@ -0,0 +1,10 @@
+var audio = new Audio('https://vincens2005.github.io/vr/Nyan%20Cat%20[original].mp3');
+
+            function play(){
+            audio.play();
+            };
+
+            function stop(){
+                audio.pause();
+            audio.currentTime = 0;
+                };
